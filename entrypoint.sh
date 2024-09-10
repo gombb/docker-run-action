0d9a3576864661b984a322a4e90d825a1a84648b
Áron Szantner <aron.szantner@gmail.com>
2024-09-10 21:54:10 +0300

    test new entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
-if [ ! -z $INPUT_USERNAME ];
-then echo $INPUT_PASSWORD | docker login $INPUT_REGISTRY -u $INPUT_USERNAME --password-stdin
+if [ ! -z $INPUT_USERNAME ]; then
+    echo $INPUT_PASSWORD | docker login $INPUT_REGISTRY -u $INPUT_USERNAME --password-stdin
 fi
 
-if [ ! -z $INPUT_DOCKER_NETWORK ];
-then INPUT_OPTIONS="$INPUT_OPTIONS --network $INPUT_DOCKER_NETWORK"
+if [ ! -z $INPUT_DOCKER_NETWORK ]; then
+    INPUT_OPTIONS="$INPUT_OPTIONS --network $INPUT_DOCKER_NETWORK"
 fi
 
-echo $INPUT_CONTEXT_VARIABLES
-
+if [[ -n "${INPUT_CONTEXT_VARIABLES}" ]]; then
+    set -a
+    source <(echo -e "$INPUT_CONTEXT_VARIABLES" | jq -r 'to_entries|map("export \(.key)=\(.value|tostring|@sh)")|.[]')
+    set +a;
+fi    
 
 exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" $INPUT_OPTIONS --entrypoint=$INPUT_SHELL $INPUT_IMAGE -c "${INPUT_RUN//$'\n'/;}"
