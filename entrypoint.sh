da9de24c7e2e48ac37da667d0dc2134d7a10897e
Áron Szantner <aron.szantner@gmail.com>
2024-09-11 09:08:46 +0300

    add test to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,4 +14,5 @@ if [[ -n "${INPUT_CONTEXT_VARIABLES}" ]]; then
     set +a;
 fi    
 
+echo $ENV
 exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" $INPUT_OPTIONS --entrypoint=$INPUT_SHELL $INPUT_IMAGE -c "${INPUT_RUN//$'\n'/;}"
