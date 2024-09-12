f192d7cc2a73f3dab11bb4c03601c3efb83ffba7
Áron Szantner <aron.szantner@gmail.com>
2024-09-12 13:59:15 +0300

    update jq

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,9 +12,8 @@ if [[ -n "${INPUT_CONTEXT_VARIABLES}" ]]; then
     while IFS="=" read -r key value; do
         INPUT_OPTIONS="$INPUT_OPTIONS -e $key=$value"
         echo DONE $key
-    done < <(echo "$INPUT_CONTEXT_VARIABLES" | jq -r 'to_entries|map("\(.key)=\(.value|tostring|@sh)")|.[]')
+    done < <(echo "$INPUT_CONTEXT_VARIABLES" | jq -r 'to_entries | map("\(.key)='\''\\(.value|tostring|@sh)'\''")|.[]')
 fi
-echo $LEARN_GRADLE_TASK
 # exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" $INPUT_OPTIONS $INPUT_IMAGE --entrypoint=$INPUT_SHELL -c "${INPUT_RUN//$'\n'/;}"
 exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" $INPUT_OPTIONS $INPUT_IMAGE
 # DOCKER_CMD="docker run -v /var/run/docker.sock:/var/run/docker.sock $INPUT_OPTIONS"
