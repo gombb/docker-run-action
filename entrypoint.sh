83ecb618ef3c4bc9c13245921d2f057f9365804d
Áron Szantner <aron.szantner@gmail.com>
2024-09-13 11:43:56 +0300

    fix ECR password leaking to logs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ ! -z $INPUT_USERNAME ]; then
-    echo $INPUT_PASSWORD | docker login $INPUT_REGISTRY -u $INPUT_USERNAME --password-stdin
+    echo $ECR_PASSWORD | docker login $INPUT_REGISTRY -u $INPUT_USERNAME --password-stdin
 fi
 
 if [ ! -z $INPUT_DOCKER_NETWORK ]; then
@@ -20,7 +20,7 @@ fi
 DOCKER_CMD="docker run -v /var/run/docker.sock:/var/run/docker.sock $INPUT_OPTIONS"
 
 if [ -n "$INPUT_RUN" ]; then
-        DOCKER_CMD="$DOCKER_CMD --entrypoint=$INPUT_SHELL"
+    DOCKER_CMD="$DOCKER_CMD --entrypoint=$INPUT_SHELL"
     DOCKER_CMD="$DOCKER_CMD $INPUT_IMAGE -c \"${INPUT_RUN//$'\n'/;}\""
 else
     DOCKER_CMD="$DOCKER_CMD $INPUT_IMAGE"
